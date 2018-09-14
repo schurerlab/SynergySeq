2d15e1dc0c6b88d96e75a9d4642baaadeafb1fb9
Vasileios Stathias <v.stathias@med.miami.edu>
2018-09-13 22:09:12 -0400

    Add Acknowledgements

--- AppR7/app7.R
+++ AppR7/app7.R
@@ -100,7 +100,8 @@ tabPanel("About",
          mainPanel(
            p("More details coming soon."),
            p("Code available at:"),
-           a("https://github.com/schurerlab/SynergySeq",target="_blank",href="https://github.com/schurerlab/SynergySeq")
+           a("https://github.com/schurerlab/SynergySeq",target="_blank",href="https://github.com/schurerlab/SynergySeq"),
+           p("Acknowledgements: This research was supported by grant U54HL127624 awarded by the National Heart, Lung, and Blood Institute through funds provided by the trans-NIH Library of Integrated Network-based Cellular Signatures (LINCS) Program (http://www.lincsproject.org/) and the trans-NIH Big Data to Knowledge (BD2K) initiative (https://datascience.nih.gov/).")
            
          )
         )
